d35cac4b9156fdeb2c75b5de4f8824fa94c54b63
Tony hart <tonyhart2020@gmail.com>
2023-06-04 12:07:15 +0700

    fix and update build gradle

--- example/lib/screens/augmented_faces.dart
+++ example/lib/screens/augmented_faces.dart
@@ -1,5 +1,3 @@
-import 'dart:typed_data';
-
 import 'package:flutter/material.dart';
 import 'package:arcore_flutter_plugin/arcore_flutter_plugin.dart';
 import 'package:flutter/services.dart';
--- lib/src/shape/arcore_shape.dart
+++ lib/src/shape/arcore_shape.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unnecessary_null_comparison
+
 import 'package:arcore_flutter_plugin/src/arcore_material.dart';
 import 'package:flutter/widgets.dart';
 
